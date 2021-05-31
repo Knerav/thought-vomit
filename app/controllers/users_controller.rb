fd1c02623e7b0a9bcef690ca4505780871a272ad
Knerav Kodolikar <knerav@gmail.com>
2021-05-31 14:49:51 +0530

    added edit template and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_whitelist)
+      flash[:notice] = "Your account was updated successfully!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_whitelist)
     if @user.save
