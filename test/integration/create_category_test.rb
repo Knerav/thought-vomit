f2b0c74d1722ff8268e51ce114d87498af4b6148
Knerav Kodolikar <knerav@gmail.com>
2021-06-15 13:52:51 +0530

    navbar categories | categories admin test | listing test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "adminuser", email: "admin@example.com", password: "adminpassword", admin: true)
+    sign_in_as(@admin_user)
+  end
   
   test "get new category form and create new category" do
     get "/categories/new"
